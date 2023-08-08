913e540724d42cf2c574e7a2d7a00d76bb4e2920
zakrnem <zakrnem@zohomail.com>
2023-08-07 22:31:44 -0600

    Style header buttons

--- src/header.js
+++ src/header.js
@@ -1,8 +1,17 @@
 export function header() {
+    const body = document.querySelector('body')
+    const content = document.getElementById('content')
     const header = document.createElement('header')
 
     const about = document.createElement('button')
     about.textContent = 'About me'
 
-    
+    const work = document.createElement('button')
+    work.textContent = 'Work'
+
+    const contact = document.createElement('button')
+    contact.textContent = 'Contact'
+
+    header.append(about, work, contact)
+    body.insertBefore(header, content)
 }
